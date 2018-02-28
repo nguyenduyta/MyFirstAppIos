33a96b4b4c6c595ec1319dd610fbbe681a24307b
Nguyen Duy Ta <tandcntt@gmail.com>
2018-03-01 01:30:40 +0900

    Create init data for table view

--- MyFirstApp/view/MealTableViewCell.swift
+++ MyFirstApp/view/MealTableViewCell.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class MealTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
 
     override func awakeFromNib() {
         super.awakeFromNib()
