8fcd77d550e8e79ffb14d50db0a70d7cb7a5c170
Nguyen Duy Ta <tandcntt@gmail.com>
2018-03-01 00:47:56 +0900

    create table view cell

---
+++ MyFirstApp/view/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  MyFirstApp
+//
+//  Created by Ta Nguyen on 2018/03/01.
+//  Copyright © 2018 Ta Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
